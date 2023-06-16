9ef362b1fd81964b227e3e49c4fcc0d77fc6c5f9
Arian <arianahmadi1@gmail.com>
2023-06-16 10:12:31 -0400

    styling and changes to photolist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import PhotoList from './components/PhotoList'; // Import the PhotoList component
 
-import PhotoListItem from './components/PhotoListItem';
+// import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
+
+// const photos = new Array(3).fill({}); // creates an array with 3 empty objects
+
 const App = () => (
   <div className="App">
-    <PhotoListItem/>
+    <PhotoList /> {/* Render the PhotoList component */}
   </div>
-)
+);
 
-export default App
+export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,41 @@
-
-import React from 'react';
-
+import React, { useState } from 'react';
+import FavIcon from './FavIcon';
 import '../styles/PhotoListItem.scss';
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  const [isFavorited, setIsFavorited] = useState(false); // new state variable
+
+  const handleFavIconClick = () => {
+    setIsFavorited(!isFavorited); // flip the isFavorited state
+  }
+
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt={`Image by ${username}`} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={profile} alt={`${username}'s profile`} />
+        <div className="photo-list__user-info">
+          <h2>{username}</h2>
+          <p>Photo ID: {id}</p>
+          <p className="photo-list__user-location">Location: {location.city}, {location.country}</p>
+        </div>
+      </div>
+      <div onClick={handleFavIconClick}> {/* Added div wrapper here */}
+        <FavIcon favorited={isFavorited} fill={isFavorited ? '#C80000' : '#EEEEEE'} /> 
+      </div>
+    </div>
+  );
 }
 
 PhotoListItem.defaultProps = {
-  "id": "1",
-  "location": {
-    "city": "Montreal",
-    "country": "Canada"
+  id: '1',
+  location: {
+    city: 'Montreal',
+    country: 'Canada',
   },
-  "imageSource": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  "username": "Joe Example",
-  "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-}
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: 'Joe Example',
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 
-export default PhotoListItem
+export default PhotoListItem;
